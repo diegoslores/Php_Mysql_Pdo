0277306fd61a2ec09d77efe27222d1aad1ff68e0
Enrique Agrasar <enrique@faiadolabs.com>
2019-04-09 21:06:19 +0200

    Preparados los tests

--- tests/AlumnoTest.php
+++ tests/AlumnoTest.php
@@ -5,4 +5,86 @@ use PHPUnit\Framework\TestCase;
 
 final class AlumnoTest extends TestCase {
 	
+	private static $log;
+	private static $profesor;
+	
+	public static function setUpBeforeClass(){
+		Logger::configure('configs/log4php-config.xml');
+		self::$log = Logger::getLogger("test");
+		self::$log->info("Comienza el test ". get_called_class());
+		self::$profesor = FactoryProfesor::getInstance()->fabrica();
+	}
+	
+	public function test_tengo_entradas_en_el_blog(): void{
+		$alumno = self::$profesor->get_alumno_by_id(626);
+		$this->assertTrue($alumno->tengo_entradas_en_el_blog());
+	}
+	
+	public function test_soy_mayor_de_edad(): void{
+		$alumno = self::$profesor->get_alumno_by_id(626);
+		$this->assertTrue($alumno->soy_mayor_de_edad());
+	}
+	
+	public function test_all_mis_entradas_en_el_blog(): void{
+		$alumno = self::$profesor->get_alumno_by_id(626);
+		$entradas = $alumno->all_mis_entradas_en_el_blog();
+		foreach($entradas as $unaEntrada){
+			$this->assertInstanceOf(Entrada::class, $unaEntrada);
+		}
+		$this->assertEquals(2, count($entradas));
+	}
+	
+	public function test_all_mis_entradas_en_el_blog_tituladas(): void{
+		$alumno = self::$profesor->get_alumno_by_id(406);
+		$entradas = $alumno->all_mis_entradas_en_el_blog_tituladas("nihil");
+		foreach($entradas as $unaEntrada){
+			$this->assertInstanceOf(Entrada::class, $unaEntrada);
+		}
+		$this->assertEquals(2, count($entradas));
+	}
+	
+	public function test_all_mis_entradas_en_el_blog_contienen(): void{
+		$alumno = self::$profesor->get_alumno_by_id(406);
+		$entradas = $alumno->all_mis_entradas_en_el_blog_contienen("architecto");
+		foreach($entradas as $unaEntrada){
+			$this->assertInstanceOf(Entrada::class, $unaEntrada);
+		}
+		$this->assertEquals(1, count($entradas));
+	}
+	
+	public function test_remove_mis_entradas_en_el_blog_tituladas(): void{
+		$alumno = self::$profesor->get_alumno_by_id(406);
+		$entradas = $alumno->all_mis_entradas_en_el_blog();
+		$this->assertEquals(3, count($entradas));
+		$alumno->remove_mis_entradas_en_el_blog_tituladas("voluptatibus");
+		$entradas = $alumno->all_mis_entradas_en_el_blog();
+		$this->assertEquals(2, count($entradas));
+		
+	}
+	
+	public function test_remove_mis_entradas_en_el_blog_contienen(): void{
+		$alumno = self::$profesor->get_alumno_by_id(405);
+		$entradas = $alumno->all_mis_entradas_en_el_blog();
+		$this->assertEquals(3, count($entradas));
+		$alumno->remove_mis_entradas_en_el_blog_contienen("provident");
+		$entradas = $alumno->all_mis_entradas_en_el_blog();
+		$this->assertEquals(1, count($entradas));
+	}
+
+	public function test_nueva_entrada_en_blog(): void{
+		$alumno = self::$profesor->get_alumno_by_id(400);
+		$titulo = "Pepe madruga";
+		$resumen = "Un día de Pepe";
+		$contenido = "Pepe es un pollo muy peculiar. Todos los días debe madrugar, si al cole quiere llegar";
+		$alumno->nueva_entrada_en_blog($titulo,$resumen,$contenido);
+		$entradas = $alumno->all_mis_entradas_en_el_blog_tituladas($titulo);
+		$this->assertEquals(1, count($entradas));
+		$nuevaEntrada = reset($entradas);
+		$this->assertEquals($titulo, $nuevaEntrada->getTitulo());
+		$this->assertEquals($resumen, $nuevaEntrada->getDescripction());
+		$this->assertEquals($contenido, $nuevaEntrada->getContent());
+		$this->assertNotNull($nuevaEntrada->getdate());
+	}
+	
+	
 }
--- tests/ProfesorTest.php
+++ tests/ProfesorTest.php
@@ -5,9 +5,93 @@ use PHPUnit\Framework\TestCase;
 
 # https://stackoverflow.com/a/39455129/1820838
 require __DIR__ . "/../src/FactoryProfesor.php";
+require 'vendor/autoload.php';
 
 final class ProfesorTest extends TestCase {
-	public function testFabricaUnProfesor(): void {
-		$this->assertNull(FactoryProfesor::getInstance()->fabrica());
+	
+	private static $log;
+	
+	public static function setUpBeforeClass(){
+		Logger::configure('configs/log4php-config.xml');
+		self::$log = Logger::getLogger("test");
+	}
+	
+	public function test_Fabrica_un_profesor(): void {
+		$this->assertIsObject(FactoryProfesor::getInstance()->fabrica());
+	}
+	
+	public function test_Get_all_alumnos_mayores_de_edad(): void {
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$result = $profesor->get_all_alumnos_mayores_de_edad();
+		
+		$this->assertIsIterable($result);
+		$this->assertEquals(613, count($result));
+		foreach($result as $unAlumno){
+			$this->assertInstanceOf(Alumno::class, $unAlumno);
+			if($unAlumno->getEmail()=="quitzon.billy@example.com"){
+				$this->fail('Este alumno de debería figurar');
+			}
+		}
+	}
+	
+	public function test_Get_all_alumnos_menores_de_edad(): void {
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$result = $profesor->get_all_alumnos_menores_de_edad();
+		
+		$this->assertIsIterable($result);
+		$this->assertEquals(387, count($result));
+		foreach($result as $unAlumno){
+			$this->assertInstanceOf(Alumno::class, $unAlumno);
+			if($unAlumno->getEmail()=="antonina.ankunding@example.com"){
+				$this->fail('Este alumno de debería figurar');
+			}
+		}
+	}
+	
+	public function test_Get_all_alumnos_con_email_incorrecto(): void {
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$result = $profesor->get_all_alumnos_con_email_incorrecto();
+		$this->assertIsIterable($result);
+		$this->assertEquals(5, count($result));
+		
+		foreach($result as $unAlumno){
+			$this->assertInstanceOf(Alumno::class, $unAlumno);
+			self::$log->info("El siguiente email es incorrecto: ".$unAlumno->getEmail());
+		}
+	}
+	
+	public function test_Alumnos_no_escribieron_en_blog(): void {
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$result = $profesor->alumnos_no_escribieron_en_blog();
+		foreach($result as $unAlumno){
+			$this->assertInstanceOf(Alumno::class, $unAlumno);
+			self::$log->info($unAlumno->getFirstName()." ".$unAlumno->getLastName()." no escribió en el blog.");
+		}
+		$this->assertEquals(0, count($result));
+	}
+	
+	public function test_Get_all_alumnos_con_email_de_dominio(): void {
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$dominio1 = "example.net";
+		$result = $profesor->get_all_alumnos_con_email_de_dominio($dominio1);
+		foreach($result as $unAlumno){
+			$this->assertInstanceOf(Alumno::class, $unAlumno);
+			self::$log->info($unAlumno->getFirstName()." ".$unAlumno->getLastName()." tiene email con dominio $dominio1.");
+		}
+		$this->assertEquals(327, count($result));
+	}
+	
+	public function test_Get_alumno_by_id(): void{
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$alumno = $profesor->get_alumno_by_id(626);
+		$this->assertInstanceOf(Alumno::class, $alumno);
+		$this->assertEquals($alumno->getEmail(), "jpouros@example.org");
+	}
+	
+	public function test_Get_alumno_by_email(): void{
+		$profesor = FactoryProfesor::getInstance()->fabrica();
+		$alumno = $profesor->get_alumno_by_email("jpouros@example.org");
+		$this->assertInstanceOf(Alumno::class, $alumno);
+		$this->assertEquals($alumno->getId(), 626);
 	}
 }
