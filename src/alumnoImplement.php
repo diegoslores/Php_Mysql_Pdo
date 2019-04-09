8580648f9b683843b181edf1c04cdd8d6d4c512a
dawMP - Sánchez Lores <A17DiegoSL1@sanclemente.local>
2019-04-09 21:56:17 +0200

    wip

---
+++ src/alumnoImplement.php
@@ -0,0 +1,75 @@
+<?php
+
+Class Alumno extends Alumno{
+
+  private $id;
+	
+	public function getId():int{
+    echo("cosas");
+  }
+
+  public function getFirstName():string{
+    echo("cosas");
+  }
+
+  public function getLastName():string{
+    echo("cosas");
+  }
+
+  public function getEmail(){
+    echo("cosas");
+  }
+
+	public function setEmail(string $pEmail){
+    echo("cosas");
+  }
+	
+	public function getBirthdate(){
+    echo("cosas");
+  }
+
+	public function getDischargeDate():DateTime{
+    echo("cosas");
+  }
+	
+	public static function get_alumno_by_id($pId){
+    echo("cosas");
+  }
+	
+	public static function get_alumno_by_email(string $pEmail){
+    echo("cosas");
+  }
+	
+	public function tengo_entradas_en_el_blog(): bool{
+    echo("cosas");
+  }
+
+	public function soy_mayor_de_edad(): bool{
+    echo("cosas");
+  }
+
+	public function all_mis_entradas_en_el_blog() : array{
+    echo("cosas");
+  }
+
+	public function all_mis_entradas_en_el_blog_tituladas(string $pPattern):array{
+    echo("cosas");
+  }
+
+	public function all_mis_entradas_en_el_blog_contienen(string $pPattern){
+    echo("cosas");
+  }
+	
+	public function remove_mis_entradas_en_el_blog_tituladas(string $pPattern){
+    echo("cosas");
+  }
+	
+	public function remove_mis_entradas_en_el_blog_contienen(string $pPattern){
+    echo("cosas");
+  }
+	
+	public function nueva_entrada_en_blog(string $pTitulo, string $pDescripcion, string $pContenido){
+    echo("cosas");
+  }
+	
+}
---
+++ src/profesorImplement.php
@@ -0,0 +1,33 @@
+<?php
+
+class Profesor extends Profesor {
+	
+	public function get_all_alumnos_mayores_de_edad(){
+    echo("cosas");
+  }
+	
+	public function get_all_alumnos_menores_de_edad(){
+    echo("cosas");
+  }
+	
+	public function get_all_alumnos_con_email_incorrecto(){
+    echo("cosas");
+  }
+
+	public function alumnos_no_escribieron_en_blog():array{
+    echo("cosas");
+  }
+	
+	public function get_all_alumnos_con_email_de_dominio(string $pDominio){
+    echo("cosas");
+  }
+	
+	public function get_alumno_by_id($pId){
+    echo("cosas");
+  }	
+
+	public function get_alumno_by_email(string $pEmail){
+    echo("cosas");
+  }
+	
+}
